0ffec2939bc02c42d8fa3b5e9d93df7d14a50a2d
Pavel Gordienko <guzmo@yandex.ru>
2024-07-29 17:34:33 +0300

    init: task3

--- task2.js
+++ task2.js
@@ -1,32 +1,31 @@
 function buildRoute(tickets) {
-    let routeMap = new Map();
-    let destinations = new Set();
-    // Заполняем мапу и набор конечных пунктов
-    tickets.forEach(({ from, to }) => {
-      routeMap.set(from, to);
-      destinations.add(to);
-    });
-    console.log(routeMap);
-    console.log(destinations);
-    // Находим начальную точку
-    let start = tickets.find(({ from }) => !destinations.has(from)).from;
-    console.log(start);
-    // Строим маршрут
-    let route = [];
-    while (start) {
-      let next = routeMap.get(start);
-      if (next) {
-        route.push({ from: start, to: next });
-      }
-      start = next;
+  let routeMap = new Map();
+  let destinations = new Set();
+  // Заполняем мапу и набор конечных пунктов
+  tickets.forEach(({ from, to }) => {
+    routeMap.set(from, to);
+    destinations.add(to);
+  });
+  console.log(routeMap);
+  console.log(destinations);
+  // Находим начальную точку
+  let start = tickets.find(({ from }) => !destinations.has(from)).from;
+  console.log(start);
+  // Строим маршрут
+  let route = [];
+  while (start) {
+    let next = routeMap.get(start);
+    if (next) {
+      route.push({ from: start, to: next });
     }
-  
-    return route;
+    start = next;
   }
-  var tickets = [
-    { from: "NY", to: "Moscow" },
-    { from: "Spb", to: "London" },
-    { from: "London", to: "NY" },
-  ];
-  console.log(buildRoute(tickets));
-  
+
+  return route;
+}
+var tickets = [
+  { from: "NY", to: "Moscow" },
+  { from: "Spb", to: "London" },
+  { from: "London", to: "NY" },
+];
+console.log(buildRoute(tickets));
---
+++ task3.js
@@ -0,0 +1,2 @@
+console.log(five(plus(one()))); // Outputs: 6
+console.log(seven(minus(two()))); // Outputs: 5
