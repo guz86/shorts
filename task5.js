3bdbbffeb1462c5e3b96cc2d3c0d11bb97bddb67
Pavel Gordienko <guzmo@yandex.ru>
2024-08-02 17:21:34 +0300

    feat: add task5

---
+++ task5.js
@@ -0,0 +1,8 @@
+function isUnique(string) {
+	// todo
+}
+
+console.log(isUnique('abcdef')) // -> true
+console.log(isUnique('1234567')) // -> true
+console.log(isUnique('abcABC')) // -> true
+console.log(isUnique('abcadef')) // -> false
